30c9e211ba41a36003b9c61b87be148574137767
Mikolaj Langner <mikilan123@gmail.com>
2021-06-23 13:42:47 +0200

    Utils for simulation added/finished

--- simulations.jl
+++ simulations.jl
@@ -10,15 +10,39 @@ choose_voters(N::Int, q::Int, position::Array{Int, 2}, replace::Bool)::Array{Arr
         [mod.(position + move .- 1, N) .+ 1 for move in sample(moves, q, replace = replace)]
 
 
-function decide(network::BitArray{2}, q::Int, position::Array{Int, 2}, replace::Bool)::Int8
+function get_votes(network::BitArray{2}, q::Int, position::Array{Int, 2},
+                   replace::Bool)::Tuple
         decisive = network[position...]
         voters = choose_voters(size(network)[1], q, position, replace)
-        votes = sum([network[voters[i]...] for i in 1:q])
-        return votes == q ? 1 : votes == 0 ? 0 : mod(decisive + rand() < ϵ, 2)
+        return sum([network[voters[i]...] for i in 1:q]), decisive
 end
 
 
-function change(network::BitArray{2}, q::Int, replace::Bool)::Int8
+function conformity(network::BitArray{2}, q::Int, position::Array{Int, 2},
+                    replace::Bool)::Int8
+        votes, decisive = get_votes(network, q, position, replace)
+        return votes == q ? 1 : votes == 0 ? 0 : decisive
+end
+
+
+function anticonformity(network::BitArray{2}, q::Int, position::Array{Int, 2},
+                        replace::Bool)::Int8
+        votes, decisive = get_votes(network, q, position, replace)
+        votes == q ? 0 : votes == 0 ? 1 : decisive
+end
+
+
+function independence(network::BitArray{2}, f::Float64, position::Array{Int, 2})::Int8
+        U = rand()
+        decisive = network[position...]
+        return U < f ? mod(decisive + 1, 2) : decisive
+end
+
+
+function change(network::BitArray{2}, q::Int, p::Float64, f::Float64,
+                independent::Bool, replace::Bool)::Int8
         position = rand(1:size(network)[1], 1, 2)
-        network[position...] = decide(network, q, position, replace)
+        network[position...] = rand() < 1 - p ? conformity(network, q, position, replace) :
+                               independent ? independence(network, f, position) :
+                               anticonformity(network, q, position, replace)
 end
