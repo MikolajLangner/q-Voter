8e9573ffa3a5797b62be4704c900e7c30a2d1acf
Mikolaj Langner <mikilan123@gmail.com>
2021-06-24 03:37:04 +0200

    First graph components added

--- app.jl
+++ app.jl
@@ -1,22 +1,48 @@
-using Dash, DashHtmlComponents, DashCoreComponents, Plotly
+using Dash, DashHtmlComponents, DashCoreComponents, Plotly, DataFrames
+include("simulations.jl")
+include("components.jl")
+
 
 app = dash()
 
-df = DataFrame(x = 1:10, y = 1:10)
+using Random
+N = 100
+network = Array{Int, 2}(reshape(bitrand(N^2), N, :))
+q = 3
+p = rand()
+f = rand()
+independent = false
+replaced = false
+
+max_step = 10^3
+opinions = [sum(network) / N^2]
+
+figure_heatmap = (data = [(x = 1:N, y = 1:N,
+                           z = [network[row, :] for row in 1:100],
+                           type = "heatmap")],)
+figure_time = (data = [(x = [0], y = [opinions])],
+               layout = (xaxis = (range = [0, max_step],), yaxis = (range = [-1, 1],),))
+
 
 app.layout = html_div() do
-    dcc_graph(id = "state"),
+    dcc_graph(id = "state", figure = figure_heatmap),
+    dcc_graph(id = "time", figure = figure_time),
     dcc_interval(id = "step")
 end
 
+
 callback!(
     app,
-    Output("state", "figure"),
-    Input("step", "n_intervals"),
-) do year
-    df.y .^= 2
-    return Plot(df, x = :x, y = :y)
+    Output("state", "extendData"),
+    Output("time", "extendData"),
+    Input("step", "n_intervals")
+) do step
+    change(network, q, p, f, independent, replaced)
+    step = step == nothing ? 1 : step
+    global opinions
+    opinions = vcat(opinions, sum(network) / N^2)
+    return ((z = [[network[row, :] for row in 1:100]],), 0, N),
+           ((x = [0:step], y = [opinions],), 0, step + 1)
 end
-
 
 run_server(app, debug=true)
--- simulations.jl
+++ simulations.jl
@@ -1,4 +1,5 @@
 using StatsBase
+export change
 
 
 const ϵ = .1
@@ -6,43 +7,43 @@ moves = [[i j] for i in -1:1 for j in -1:1]
 splice!(moves, 5)
 
 
-choose_voters(N::Int, q::Int, position::Array{Int, 2}, replace::Bool)::Array{Array{Int, 2}, 1} =
-        [mod.(position + move .- 1, N) .+ 1 for move in sample(moves, q, replace = replace)]
+choose_voters(N::Int, q::Int, position::Array{Int, 2}, replaced::Bool)::Array{Array{Int, 2}, 1} =
+        [mod.(position + move .- 1, N) .+ 1 for move in sample(moves, q, replace = replaced)]
 
 
-function get_votes(network::BitArray{2}, q::Int, position::Array{Int, 2},
-                   replace::Bool)::Tuple
+function get_votes(network::AbstractArray, q::Int, position::Array{Int, 2},
+                   replaced::Bool)::Tuple
         decisive = network[position...]
-        voters = choose_voters(size(network)[1], q, position, replace)
+        voters = choose_voters(size(network)[1], q, position, replaced)
         return sum([network[voters[i]...] for i in 1:q]), decisive
 end
 
 
-function conformity(network::BitArray{2}, q::Int, position::Array{Int, 2},
-                    replace::Bool)::Int8
-        votes, decisive = get_votes(network, q, position, replace)
+function conformity(network::AbstractArray, q::Int, position::Array{Int, 2},
+                    replaced::Bool)::Int8
+        votes, decisive = get_votes(network, q, position, replaced)
         return votes == q ? 1 : votes == 0 ? 0 : decisive
 end
 
 
-function anticonformity(network::BitArray{2}, q::Int, position::Array{Int, 2},
-                        replace::Bool)::Int8
-        votes, decisive = get_votes(network, q, position, replace)
+function anticonformity(network::AbstractArray, q::Int, position::Array{Int, 2},
+                        replaced::Bool)::Int8
+        votes, decisive = get_votes(network, q, position, replaced)
         votes == q ? 0 : votes == 0 ? 1 : decisive
 end
 
 
-function independence(network::BitArray{2}, f::Float64, position::Array{Int, 2})::Int8
+function independence(network::AbstractArray, f::Float64, position::Array{Int, 2})::Int8
         U = rand()
         decisive = network[position...]
         return U < f ? mod(decisive + 1, 2) : decisive
 end
 
 
-function change(network::BitArray{2}, q::Int, p::Float64, f::Float64,
-                independent::Bool, replace::Bool)::Int8
+function change(network::AbstractArray, q::Int, p::Float64, f::Float64,
+                independent::Bool, replaced::Bool)::Int8
         position = rand(1:size(network)[1], 1, 2)
-        network[position...] = rand() < 1 - p ? conformity(network, q, position, replace) :
+        network[position...] = rand() < 1 - p ? conformity(network, q, position, replaced) :
                                independent ? independence(network, f, position) :
-                               anticonformity(network, q, position, replace)
+                               anticonformity(network, q, position, replaced)
 end
